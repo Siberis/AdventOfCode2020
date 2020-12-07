5e6e3e51fb05b004baacda5ccea0d60bc0cca674
Siberis <siberis@outlook.com>
2020-12-07 07:42:26 +0100

    Day5 - 2 Stars

---
+++ Day5/Day5.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day5
+{
+    public class Day5
+    {
+
+        public static int Star1(string rules)
+        {
+            var row = rules[0..7];
+            var col = rules[7..];
+            int minRow = 0;
+            int maxRow = 127;
+            int minCol = 0;
+            int maxCol = 7;
+            int height = 64;
+            int width = 4;
+            foreach (var r in row)
+            {
+                if (r == 'F')
+                {
+                    maxRow -= height;
+                }
+                else
+                if (r == 'B')
+                {
+                    minRow += height;
+                }
+                height /= 2;
+            }
+            foreach (var c in col)
+            {
+                if (c == 'L')
+                {
+                    maxCol -= width;
+                }
+                else
+                if (c == 'R')
+                {
+                    minCol += width;
+                }
+                width /= 2;
+            }
+            return minRow * 8 + minCol;
+        }
+    }
+}
+
+
---
+++ Day5/Day5Test.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils;
+using Xunit;
+
+namespace Day5
+{
+    public class Day5Test
+    {
+        [Fact]
+        public async Task Test1()
+        {
+            Assert.Equal(357, Day5.Star1("FBFBBFFRLR"));
+            Assert.Equal(567, Day5.Star1("BFFFBBFRRR"));
+            Assert.Equal(119, Day5.Star1("FFFBBBFRRR"));
+            Assert.Equal(820, Day5.Star1("BBFFBBFRLL"));
+        }
+        [Fact]
+        public async Task Star1()
+        {
+            var lines = await ReadUtils.ReadAllLines("./Day5Part1Input.txt").ConfigureAwait(false);
+            var seats = lines.Select(e => Day5.Star1(e)).OrderBy(e => e).ToArray();
+            for (int i = 1; i < seats.Length; i++)
+            {
+                if (seats[i - 1] + 2 == seats[i])
+                {
+                    Assert.Equal(623, seats[i] - 1);
+                    return;
+                }
+            }
+            Assert.False(true);
+        }
+
+    }
+}
