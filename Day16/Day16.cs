9f62f89712df2064735f537154d786e8beef791d
Siberis <siberis@outlook.com>
2020-12-16 07:04:11 +0100

    Day 16 - 2Stars

--- Day16/Day16.cs
+++ Day16/Day16.cs
@@ -9,7 +9,139 @@ namespace Day16
     {
         public static decimal Star1(string[] lines)
         {
-            return 0;
+            var i = 0;
+            Dictionary<string, List<(int, int)>> categories = ParseCategories(lines, ref i);
+            i++;
+            List<int> myValues = new List<int>();
+            ParseMyTicket(lines, ref i, ref myValues);
+            i++;
+            var sum = 0;
+            while (i < lines.Length && lines[i].Trim().Length != 0)
+            {
+                if (lines[i].StartsWith("nearby tickets"))
+                {
+                    i++;
+                    continue;
+                }
+                foreach (var t in lines[i].Split(",").Select(e => int.Parse(e)).ToList())
+                {
+                    if (categories.Values.SelectMany(e => e).Any(v => v.Item1 <= t && v.Item2 >= t))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        sum += t;
+                    }
+                }
+                i++;
+            }
+            i++;
+
+            return sum;
+        }
+        public static decimal Star2(string[] lines, string target)
+        {
+            var i = 0;
+            Dictionary<string, List<(int, int)>> categories = ParseCategories(lines, ref i);
+            i++;
+            List<int> myValues = new List<int>();
+            ParseMyTicket(lines, ref i, ref myValues);
+            i++;
+            var potentials = new Dictionary<int, List<string>>();
+            var keys = categories.Keys.ToList();
+            var nerby = new List<List<int>>();
+            while (i < lines.Length && lines[i].Trim().Length != 0)
+            {
+                if (lines[i].StartsWith("nearby tickets"))
+                {
+                    i++;
+                    continue;
+                }
+                var ticket = lines[i].Split(",").Select(e => int.Parse(e)).ToList();
+
+                if (categories.Values.Any(x => ticket.All(v => x.Any(p => p.Item1 <= v && p.Item2 >= v))))
+                {
+                    nerby.Add(ticket);
+                }
+                i++;
+            }
+            for (int y = 0; y < myValues.Count; y++)
+            {
+                var vals = nerby.Select(v => v[y]);
+                for (int x = 0; x < categories.Count; x++)
+                {
+                    var potential = categories[keys[x]];
+                    if (vals.All(v => potential.Any(p => p.Item1 <= v && p.Item2 >= v)))
+                    {
+                        if (!potentials.ContainsKey(y))
+                        {
+                            potentials[y] = new List<string>();
+                        }
+                        potentials[y].Add(keys[x]);
+                    }
+                }
+            }
+            while (!potentials.Values.All(e => e.Count == 1))
+            {
+                potentials.Values.Where(e => e.Count == 1).ToList().ForEach(min =>
+                {
+                    foreach (var item in potentials)
+                    {
+                        if (item.Value.Count == 1)
+                        {
+                            continue;
+                        }
+                        item.Value.Remove(min[0]);
+                    }
+                });
+            }
+            decimal sum = 1;
+
+            for (int x = 0; x < potentials.Count; x++)
+            {
+                if (potentials[x][0].StartsWith(target))
+                {
+                    sum *= myValues[x];
+                }
+            }
+
+            return sum;
+        }
+
+        private static void ParseMyTicket(string[] lines, ref int i, ref List<int> myValues)
+        {
+            while (lines[i].Trim().Length != 0)
+            {
+                if (lines[i].StartsWith("your ticket"))
+                {
+                    i++;
+                    continue;
+                }
+                myValues = lines[i].Split(",").Select(e => int.Parse(e)).ToList();
+                i++;
+            }
+        }
+
+        private static Dictionary<string, List<(int, int)>> ParseCategories(string[] lines, ref int i)
+        {
+            var categories = new Dictionary<string, List<(int, int)>>();
+            while (lines[i].Trim().Length != 0)
+            {
+                var p1 = lines[i].Split(": ");
+                if (!categories.ContainsKey(p1[0]))
+                {
+                    categories.Add(p1[0], new List<(int, int)>());
+                }
+                p1[1].Split(" or ").ToList().ForEach(e =>
+                {
+                    var x = e.Split("-");
+                    categories[p1[0]].Add((int.Parse(x[0]), int.Parse(x[1])));
+                });
+                i++;
+            }
+
+            return categories;
         }
     }
 }
--- Day16/Day16Test.cs
+++ Day16/Day16Test.cs
@@ -14,13 +14,25 @@ namespace Day16
         public async Task Test1()
         {
             var lines = await ReadUtils.ReadAllLines("./Day16Part1Test1.txt").ConfigureAwait(false);
-            Assert.Equal(0, Day16.Star1(lines));
+            Assert.Equal(71, Day16.Star1(lines));
         }
         [Fact]
         public async Task Star1()
         {
             var lines = await ReadUtils.ReadAllLines("./Day16Part1Input.txt").ConfigureAwait(false);
-            Assert.Equal(0, Day16.Star1(lines));
+            Assert.Equal(23036, Day16.Star1(lines));
+        }
+        [Fact]
+        public async Task Test2()
+        {
+            var lines = await ReadUtils.ReadAllLines("./Day16Part2Test1.txt").ConfigureAwait(false);
+            Assert.Equal(12, Day16.Star2(lines, "class"));
+        }
+        [Fact]
+        public async Task Star2()
+        {
+            var lines = await ReadUtils.ReadAllLines("./Day16Part1Input.txt").ConfigureAwait(false);
+            Assert.Equal(1909224687553, Day16.Star2(lines, "departure"));
         }
     }
 }
